179ea47f9d3a2aec0db0c9c9940dd7f95934a654
rylemacaraig <rylemacaraig@gmail.com>
2019-05-29 12:17:22 -0400

    -fixed errors and reformatted code -changed naming convention on edit text and button variables -worked on toast message functionality with edit text and clear button -working on getting radio button, spinner, and checkbox to work on toast message

--- Ryle_Macaraig_A1/app/src/main/java/com/example/ryle_macaraig_a1/MainActivity.java
+++ Ryle_Macaraig_A1/app/src/main/java/com/example/ryle_macaraig_a1/MainActivity.java
@@ -4,7 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 /**
  * Created by 991435278: Ryle Cheyene Macaraig
@@ -14,67 +19,150 @@ import android.widget.EditText;
 //implements View.OnClickListener
 public class MainActivity extends AppCompatActivity {
 
-    Button submit, cancel;
-    EditText name, email, phone;
+    //variables to call all objects
+    Button btnSubmit, btnCancel;
+    EditText edtName, edtEmail, edtPhone;
+    RadioGroup rdoApp;
+    RadioButton rdoYes, rdoNo, rdoMaybe;
+    //spinner??????
+    CheckBox chkGuitar, chkFootball, chkSinging, chkChess, chkHorseRiding, chkReading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        submit = findViewById(R.id.btn_submit);
-        //submit.setOnClickListener(this);
+        //Submit button
+        btnSubmit = findViewById(R.id.btn_submit);
 
-        cancel = findViewById(R.id.btn_cancel);
-        //cancel.setOnClickListener(this);
+        //Cancel button
+        btnCancel = findViewById(R.id.btn_cancel);
 
-        name = findViewById(R.id.edt_name);
-        email = findViewById(R.id.edt_email);
-        phone = findViewById(R.id.edt_phone);
+        //Edit Texts
+        edtName = findViewById(R.id.edt_name);
+        edtEmail = findViewById(R.id.edt_email);
+        edtPhone = findViewById(R.id.edt_phone);
 
+        //RadioGroup
+        rdoApp = findViewById(R.id.rdo_app);
 
-        //submit button
-        submit.setOnClickListener(new View.OnClickListener() {
+        //Radio Buttons
+        rdoYes = findViewById(R.id.rdo_yes);
+        rdoNo = findViewById(R.id.rdo_no);
+        rdoMaybe = findViewById(R.id.rdo_maybe);
+
+        //Checkboxes
+        chkGuitar = findViewById(R.id.chk_guitar);
+        chkFootball = findViewById(R.id.chk_football);
+        chkSinging = findViewById(R.id.chk_singing);
+        chkChess = findViewById(R.id.chk_chess);
+        chkHorseRiding = findViewById(R.id.chk_horse_riding);
+        chkReading = findViewById(R.id.chk_reading);
+
+        //submit button clicked functionality
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                //error check method
+                errorCheck();
+
+                //calling radio buttons when clicked
+                rdoYes = findViewById(rdoApp.getCheckedRadioButtonId());
+                rdoNo = findViewById(rdoApp.getCheckedRadioButtonId());
+                rdoMaybe = findViewById(rdoApp.getCheckedRadioButtonId());
+
+                //getting input from edit texts
+                String name = edtName.getText().toString();
+                String email = edtEmail.getText().toString();
+                String phone = edtPhone.getText().toString();
+
+                //toast message to output users inputs
+                Toast.makeText(MainActivity.this, "Please verify your details." + "\nRecommendations: " + "....." + "\nName : " + name + "\nEmail : " + email + " \nPhone : " + phone + "\nThanks for the feedback.", Toast.LENGTH_LONG).show();
+
+
+                //check box functionality
+//                String selectedHobbies = "";
+//
+//                if (chkGuitar.isChecked()) {
+//                    selectedHobbies += chkGuitar.getText().toString() + "\n";
+//                }
+//                else{
+//                    selectedHobbies += chkGuitar.getText().toString() + "\n";
+//                }
+//
+//                if (chkFootball.isChecked()) {
+//                    selectedHobbies += chkFootball.getText().toString() + "\n";
+//                }
+//                else{
+//                    selectedHobbies += chkFootball.getText().toString() + "\n";
+//                }
+//
+//                if (chkSinging.isChecked()) {
+//                    selectedHobbies += chkSinging.getText().toString() + "\n";
+//                }
+//                else{
+//                    selectedHobbies += chkSinging.getText().toString() + "\n";
+//                }
+//
+//                if (chkChess.isChecked()) {
+//                    selectedHobbies += chkChess.getText().toString() + "\n";
+//                }
+//                else{
+//                    selectedHobbies += chkChess.getText().toString() + "\n";
+//                }
+//
+//                if (chkHorseRiding.isChecked()) {
+//                    selectedHobbies += chkHorseRiding.getText().toString() + "\n";
+//                }
+//                else{
+//                    selectedHobbies += chkHorseRiding.getText().toString() + "\n";
+//                }
+//
+//                if (chkReading.isChecked()) {
+//                    selectedHobbies += chkReading.getText().toString() + "\n";
+//                }
+//                else{
+//                    selectedHobbies += chkReading.getText().toString() + "\n";
+//                }
+
+                // display it as Toast to the user
+
+                //Toast.makeText(MainActivity.this, "Please verify your details" + "\n Recommendation: " + yes + no +  maybe +"\n CheckBox Choices: \n "+ selectedHobbies , Toast.LENGTH_LONG).show();
+
+
             }
+
+
         });
 
-        //cancel button
-        cancel.setOnClickListener(new View.OnClickListener() {
+        //cancel button clicked functionality
+        btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                //clear all edit texts
+                edtName.getText().clear();
+                edtEmail.getText().clear();
+                edtPhone.getText().clear();
+
             }
         });
-
-
 
     }
 
-
-
-
-
-
-//    @Override
-//    public void onClick(View v){
-//
-//        if (v.getId() == submit.getId()){
-//
-//        }
-//    }
-
-//    @Override
-//    public void onClick(View v){
-//
-//        if (v.getId() == cancel.getId()){
-//
-//        }
-//    }
-
-
-
+    //error check if users does not input name, email, or phone number
+    private boolean errorCheck() {
+        if (edtName.length() == 0) {
+            edtName.setError("Name is required");
+        }
+        if (edtEmail.length() == 0) {
+            edtEmail.setError("Email is required");
+        }
+        if (edtPhone.length() == 0) {
+            edtPhone.setError("Phone number is required");
+        }
+        return true;
+    }
 
 }
