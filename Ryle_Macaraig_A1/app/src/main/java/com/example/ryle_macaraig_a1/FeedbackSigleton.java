0b0cc74f62b1c59681612d80a4b6650a7d3687ec
rylemacaraig <rylemacaraig@gmail.com>
2019-06-11 22:36:28 -0400

    -created a new class (FeedbackController and FeedbackSigleton) -created getters, setters, toString, and constructor for Feedback class

---
+++ Ryle_Macaraig_A1/app/src/main/java/com/example/ryle_macaraig_a1/FeedbackSigleton.java
@@ -0,0 +1,20 @@
+package com.example.ryle_macaraig_a1;
+
+/**
+ * Created by 991435278: Ryle Cheyene Macaraig
+ * on 2019-06-11.
+ */
+
+//an ArrayList to hold all the Feedback objects
+//getFeedbackByEmail() method which will return Feedback
+// object associated with given email
+
+//addFeedback() method to insert a new Feedback object into the ArrayList.
+
+public class FeedbackSigleton {
+
+
+
+
+
+}
--- Ryle_Macaraig_A1/app/src/main/java/com/example/ryle_macaraig_a1/model/Feedback.java
+++ Ryle_Macaraig_A1/app/src/main/java/com/example/ryle_macaraig_a1/model/Feedback.java
@@ -6,11 +6,88 @@ package com.example.ryle_macaraig_a1.model;
  */
 public class Feedback {
 
+    String recommendation, hobbies, frequency, name,
+            email, phoneNumber, creditScore;
 
+    // constructor, getters, setters and toString()
 
+    public Feedback(String recommendation, String hobbies, String frequency,
+                    String name, String email, String phoneNumber, String creditScore) {
+        this.recommendation = recommendation;
+        this.hobbies = hobbies;
+        this.frequency = frequency;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.creditScore = creditScore;
+    }
 
+    public String getRecommendation() {
+        return recommendation;
+    }
 
+    public String getHobbies() {
+        return hobbies;
+    }
 
+    public String getFrequency() {
+        return frequency;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getCreditScore() {
+        return creditScore;
+    }
+
+    public void setRecommendation(String recommendation) {
+        this.recommendation = recommendation;
+    }
+
+    public void setHobbies(String hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    public void setFrequency(String frequency) {
+        this.frequency = frequency;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setCreditScore(String creditScore) {
+        this.creditScore = creditScore;
+    }
+
+    @Override
+    public String toString() {
+        return "Feedback{" +
+                "recommendation='" + recommendation + '\'' +
+                ", hobbies='" + hobbies + '\'' +
+                ", frequency='" + frequency + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", creditScore='" + creditScore + '\'' +
+                '}';
+    }
 }
