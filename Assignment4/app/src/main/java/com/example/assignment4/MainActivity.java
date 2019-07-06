c668b28b509adc4ee92afbe3a6ae7c7e0ca9c458
rylemacaraig <rylemacaraig@gmail.com>
2019-07-06 14:02:28 -0400

    -refered UI widgets -selected on click button for selectProvince. Created a dialog box

--- Assignment4/app/src/main/java/com/example/assignment4/MainActivity.java
+++ Assignment4/app/src/main/java/com/example/assignment4/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.assignment4;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvProvince, tvCity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //refer to UI widgets method
+        referWidgets();
     }
+
+    //call all UI widgets
+    private void referWidgets() {
+
+        tvProvince = findViewById(R.id.tv_province);
+        tvCity = findViewById(R.id.tv_city);
+    }
+
+
+    public void selectProvince(View view) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Would you like to open Second Activity?")
+        .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        })
+        .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
 }
